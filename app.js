5594de0bbc12c693544dcda4016078871e61f9d5
Nafsika Abatzi <61847066+nafsika24@users.noreply.github.com>
2021-04-14 23:15:28 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
+
+const app = express();
+
+// Connect Database
+connectDB();
+app.get('/',(req,res) => res.send('Welcome'))
+app.get('/api',(req,res) => res.send('Api running'))
+// Init Middleware
+app.use(express.json());
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+
+
+
+
+
+module.exports = app;
