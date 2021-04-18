e21c2f0c473e6765b5c50d6a01fafa4350663d75
Nafsika Abatzi <61847066+nafsika24@users.noreply.github.com>
2021-04-18 04:03:38 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ app.use(express.json());
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-
-
-
-
-
+app.use('/api/logout',require('./routes/api/logout'));
 
 module.exports = app;
