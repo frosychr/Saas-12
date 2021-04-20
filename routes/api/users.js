3fea89ff0d84dfc83b91fba3b529d1766d2430b8
Nafsika Abatzi <61847066+nafsika24@users.noreply.github.com>
2021-04-20 19:25:08 +0300

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -14,68 +14,61 @@ const User = require('../../models/User');
 // @access   Public
 
 router.post('/',
-[
-    check('firstname','Firstname is required').not().isEmpty(),
-    check('lastname','Lastname is required').not().isEmpty(),
-    check('email','Please include a valid email').isEmail(),
-    check('password', 'Please enter a valid password').isLength({min : 6})
-],
-async (req,res) =>{ 
-   const errors = validationResult(req);
-   if(!errors.isEmpty()){
-       return res.status(400).json({errors: errors.array() });
-   }
-
-
-   const {firstname,lastname,email,password} = req.body;
-   try {
-     let user = await User.findOne({ email });
-     if (user) {
-       return res
-         .status(400)
-         .json({ errors: [{ msg: 'User already exists' }] });
-     }
-     const avatar = normalize(
-        gravatar.url(email, {
-          s: '200',
-          r: 'pg',
-          d: 'mm'
-        }),
-        { forceHttps: true }
-      );
-      user = new User({
-          firstname,
-          lastname,
-        email,
-        password
-      });
-      const salt = await bcrypt.genSalt(10);
-
-      // user takes the password and creates a hash to put the user password
-      user.password = await bcrypt.hash(password, salt);
-      await user.save();
-      //res.send('User registered');
-
-      // payload
-      const payload = {
-        user:{
-            id: user.id
+    [
+        check('firstname','Firstname is required').not().isEmpty(),
+        check('lastname','Lastname is required').not().isEmpty(),
+        check('email','Please include a valid email').isEmail(),
+        check('password', 'Please enter a valid password').isLength({min : 6})
+    ],
+    async (req,res) =>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array() });
         }
-      }
+        
+        const {firstname,lastname,email,password} = req.body;
+        try {
+            let user = await User.findOne({ email });
+            if (user) {
+                return res
+                    .status(400)
+                    .json({ errors: [{ msg: 'User already exists' }] });
+            }
 
-      jwt.sign(payload, config.get('jwtSecret'),{expiresIn:360000 },
-      (err,token)=>{
-          if(err) throw err;
-          res.json({token});
-      
+            user = new User({
+                firstname,
+                lastname,
+                email,
+                password
+            });
+
+            const salt = await bcrypt.genSalt(10);
+
+            // user takes the password and creates a hash to put the user password
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+            //res.send('User registered');
+
+            // payload
+            const payload = {
+                user:{
+                    id: user.id
+                }
+            }
+
+            jwt.sign(payload, config.get('jwtSecret'),{expiresIn:360000 },
+                (err,token)=>{
+                    if(err) throw err;
+                    res.json({token});
+
+
+                });
+        }catch(err){
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+
 
     });
-}catch(err){
-       console.error(err.message);
-       res.status(500).send('Server error');
-   }
-
- 
-});
 
 module.exports = router;
