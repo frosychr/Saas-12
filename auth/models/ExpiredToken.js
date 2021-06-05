c0e5f905eb960420f0663199f06043fe7d91dc4a
frosychr <el17178@mail.ntua.gr>
2021-06-05 19:51:38 +0300

    delete everything

--- auth/models/ExpiredToken.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const ExpiredTokenSchema = new mongoose.Schema({
-    tokenID:{
-        type: String,
-        required: true
-    }
-});
-module.exports = ExpiredToken = mongoose.model('expiredtoken', ExpiredTokenSchema);
