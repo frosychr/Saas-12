d2f20d78673d5e73d459ed00ee7adb5a129a09a2
nafsika24 <el17198@mail.ntua.gr>
2021-07-11 11:04:02 +0300

    minor change

--- Microservices/client/app.js
+++ Microservices/client/app.js
@@ -86,7 +86,7 @@ mongoose
     .connect(MONGODB_URI)
     .then(result => {
         console.log('Mongodb Connected');
-        app.listen(3001 || process.env.PORT);
+        app.listen(process.env.PORT || 3001);
     })
 
 .catch(err => {
